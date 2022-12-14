dca8eb134d32d2cb185886a8e43f65e67d361684
vladislav shakhov <shahov199@gmail.com>
2022-12-14 17:28:10 +0300

    Example 006 bestUser

---
+++ Example_006_BestUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter userName");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurrah, this is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
